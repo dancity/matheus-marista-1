e88a8c754a4184075ab903d33e6c30097e08916a
André Cavalcanti Ribeiro <andre_ribeiro97@hotmail.com>
2021-02-18 23:32:17 -0300

    feat footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container, ThanksContainer, InfosContainer } from "./styles";
+
+export default function Footer() {
+  return (
+    <Container>
+      <ThanksContainer>
+        <h1>Obrigado por fazer parte dessa história!</h1>
+      </ThanksContainer>
+      <InfosContainer>
+        <p>Email: matheusleaorangel@gmail.com</p>
+        <p>Telefone: (21) 98340-9000</p>
+        <a href="https://www.linkedin.com/in/matheusleaorangel/">Meu Linkedin</a>
+      </InfosContainer>
+    </Container>
+  );
+}
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+  align-items: center;
+  justify-content: center;
+  min-height: 280px;
+  min-width: 100%;
+
+  h1 {
+    color: #000;
+    font-family: Raleway-Medium;
+    font-size: 28px;
+    margin: 10px;
+  }
+`;
+
+export const ThanksContainer = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background: #D24A00;
+  width: 100%;
+`;
+
+export const InfosContainer = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  background: #2C2C2C;
+  width: 100%;
+  padding-right: 30px;
+
+  p {
+    color: #fff;
+    font-family: Raleway-Medium;
+    margin-top: 6px;
+  }
+
+  a {
+    color: #fff;
+    font-family: Raleway-Medium;
+    margin-top: 6px;
+  }
+`;
+
